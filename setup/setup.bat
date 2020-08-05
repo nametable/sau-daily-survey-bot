ce34fa7b3a3216621fcdb4494a8a97e4e5d84fc6
AlexOConnorHub <43659595+AlexOConnorHub@users.noreply.github.com>
2020-08-05 13:21:15 -0400

    Adding .json file setup

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,1 +1,5 @@
 pip3 install requests furl bs4 lxml
+set /p username="Please enter your SAU username: "
+set /p passphrase="Please enter your SAU passphrase: "
+printf "{\n    \"username\":\"%s\"\n" %username% > %~dpnx1\..\secrets.json
+printf "    \"password\":\"%s\"\n}" %passphrase% >> %~dpnx1\..\secrets.json
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pip3 install requests furl bs4 lxml
+echo Please enter your SAU username: 
+read username
+echo Please enter your SAU passphrase: 
+read passphrase
+printf "{\n    \"username\":\"%s\",\n" $username > $DIR/../secrets.json
+printf "    \"password\":\"%s\"\n}" $passphrase >> $DIR/../secrets.json
+
